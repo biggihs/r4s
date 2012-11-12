a3cdc52d55e361d9f2e424ff6092df30c1090361
birgir <birgir@T420.(none)>
2012-11-12 10:16:07 +0000

    Total rewrite. Works.

--- lib/r4s.rb
+++ lib/r4s.rb
@@ -1,16 +1,64 @@
 require "r4s/version"
+require 'json'
 
-module R4S
+class R4S
+  SSES = {}
+
+  def self.add_stream(response,key="none")
+
+    if !R4S::SSES.keys.include?(key)
+      R4S::SSES[key] = {}
+      key_count = 0
+    else
+      key_count = R4S::SSES[key].keys.count
+    end
+
+    sse = R4S::SSE.new(response,key,key_count)
+    R4S::SSES[key][key_count] = sse
+
+    return sse
+
+  end
+
+  def self.push_data(key,data,options={})
+    if R4S::SSES.keys.include?(key)
+      R4S::SSES[key].each do |k,sse|
+        sse.write(data,options)
+      end
+    end
+  end
+
   class SSE
-    def initialize io
-      @io = io
+
+    def initialize response, key, id
+      response.headers['Content-Type'] = 'text/event-stream'
+      @response = response
+      @id = id
+      @key = key
+      @io = response.stream
     end
 
     def write object, options = {}
-      options.each do |key,value|
-        @io.write "#{key}: #{value}"
+      options.each do |k,v|
+        @io.write "#{k}: #{v}\n"
       end
       @io.write "data: #{JSON.dump(object)}\n\n"
+    end
+
+    def start
+      begin
+        while !@io.closed?; 
+          sleep 30
+        end
+      rescue
+      ensure
+        R4S::SSES[@key][@id].close 
+        R4S::SSES[@key].delete(@id)
+      end
+    end
+
+    def closed?
+      @io.closed?
     end
 
     def close
--- lib/r4s/version.rb
+++ lib/r4s/version.rb
@@ -1,3 +1,3 @@
 module R4s
-  VERSION = "0.0.1"
+  VERSION = "1.0.0"
 end
