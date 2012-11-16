a8aa47c9dee64f7f3e2ff1dd676a3bbf0ff0721a
birgir <birgir@T420.(none)>
2012-11-16 12:27:14 +0000

    Bugfix. If the user opened two tabs with the same stream then each tab would close the connection for the other tab and open a new one for themself.

--- lib/r4s.rb
+++ lib/r4s.rb
@@ -1,31 +1,37 @@
 require 'thread_safe'
 require 'json'
+
+#TODO:: Figure out if there is ANY way to share a sse-connection (true broadcast)
 module R4S
+  #SSES Is a global hash that stores all the sse-connections. Accessable between threads.
+  #Format: SSES["key"]["session.id"][#number]
+  #      : The #number is because a user might open the same page in two tabs, in which 
+  #      : the browser needs a sse-connection for each tab.
   SSES = ThreadSafe::Hash.new
 
-  #The session_id is used to identify users who already have a connection
   #The key is used to group "identical" sse-streams so you can push data to a group of streams
+  #The session_id is used to identify users who already have a connection
   def self.add_stream(response,session,key="none")
 
     id = session["session_id"]
 
-    R4S::SSES[key] = ThreadSafe::Hash.new unless R4S::SSES.keys.include?(key)
-
-    if R4S::SSES[key].keys.include?(id)
-      sse = R4S::SSES[key][id]
-      sse.close
+    unless R4S::SSES.keys.include?(key)
+      R4S::SSES[key] = ThreadSafe::Hash.new
+      R4S::SSES[key][id] = ThreadSafe::Array.new
     end
 
     sse = R4S::SSE.new(response,key,id)
-    R4S::SSES[key][id] = sse
+    R4S::SSES[key][id] << sse
 
     return sse
   end
 
   def self.push_data(key,data,options={})
     if R4S::SSES.keys.include?(key)
-      R4S::SSES[key].each do |k,sse|
-        sse.write(data,options)
+      R4S::SSES[key].each do |k,client|
+        client.each do |sse|
+          sse.write(data,options)
+        end
       end
     end
   end
@@ -50,14 +56,12 @@ module R4S
     def start
       begin
         while !@io.closed?; 
-          sleep 30
+          sleep 10  #I don't know if this is a good idea or if I should skip "sleep" entirely.
+          self.write({},{}) # If !@io.closed? gives a false positive then this crashes and forces exit
         end
       rescue
       ensure
-        #it might have been reopened
-        unless !R4S::SSES[@key][@id].closed?
-          R4S::SSES[@key].delete(@id)
-        end
+        R4S::SSES[@key][@id].delete(self)
       end
     end
 
